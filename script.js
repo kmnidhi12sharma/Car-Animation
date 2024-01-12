769111373289cab6ab4f3e33f0742d96a2307938
KM NIDHI SHARMA <75654309+kmnidhi12sharma@users.noreply.github.com>
2024-01-12 11:33:55 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+var audio=document.createElement('audio');
+audio.setAttribute('src','Car Aminated/sound.mp3');
+audio.setAttribute('autoplay','Car Aminated/sound.mp3');
+audio.loop=true;
+audio.play();
